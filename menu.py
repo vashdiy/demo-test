f8632df92c7f935ca4382dd02210a7ffdc83ec7c
vashdiy2 <vashdiy2020@gmail.com>
2025-09-08 20:07:04 +0300

    add hii

---
+++ menu.py
@@ -0,0 +1,15 @@
+__author__: 'Yuval Vashdi'
+
+def show_menu():
+   print("=" * 30)
+   print("Select an option:")
+   print("1. Start")
+   print("2. Exit")
+   print("=" * 30)
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
